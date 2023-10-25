b2f9070627d1fc1a0457027ec38eac3ed5f81565
Ahmed Khalaf <pxlshpr@gmail.com>
2023-10-25 19:47:20 +0500

    initial commit

--- Package.swift
+++ Package.swift
@@ -5,17 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "PrepBiometrics",
+    platforms: [
+        .iOS(.v17)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PrepBiometrics",
             targets: ["PrepBiometrics"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pxlshpr/PrepShared", from: "0.0.132"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PrepBiometrics"),
+            name: "PrepBiometrics",
+            dependencies: [
+                .product(name: "PrepShared", package: "PrepShared"),
+            ]
+        ),
         .testTarget(
             name: "PrepBiometricsTests",
             dependencies: ["PrepBiometrics"]),
